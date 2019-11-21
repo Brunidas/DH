b26bbbf25fe3e7be67dbfadac9a7305f8973c47d
Bruno Fuentes <bruno.e.fuentes97@gmail.com>
2019-11-21 15:44:42 -0300

    MODIFIED: home, header y footer

--- 002home.php
+++ 002home.php
@@ -17,20 +17,36 @@
 
     <?php include_once "components/header.php"; ?>
 
-    <div class="container">
-        <div class="_bf_titulos">
-            <h2>Cartilla de</h2>
-            <h1 class="">Especialidades</h1>
+    <div class="container p-4 p-md-5">
+        <div class="_bf_titulos-mobile row mb-4 ">
+            <div class="col-12 d-flex justify-content-center">
+                <h2 class="h1">Cartilla De</h2>
+            </div>
+
+            <div class="col-12 d-flex justify-content-center">
+                <h2 class="h1">Especialidades</h2>
+            </div>
+
         </div>
 
 
+        <div class="_bf_titulos mt-5 mb-5">
+            <h2>Cartilla De</h2>
+            <h1 class="display-1 ">Especialidades</h1>
+        </div>
 
-        <div class="row d-flex flex-wrap justify-content-around">
+        <hr>
+
+
+
+
+
+        <div class="row">
 
             <?php for($i=1;$i<=$cantidadEspecialidades;$i++): ?>
 
-                <div class="col-sm-6 col-md-5 col-lg-4 d-flex justify-content-center">
-                    <button class="_bf_especialidades" type="submit">
+                <div class="col-12 d-flex justify-content-center col-sm-6 col-md-4 mb-3 while">
+                    <button class="_bf_especialidad pt-3 pb-3" type="submit">
                         Especialidad <?=$i;?>
                     </button>
                 </div>
--- components/footer.php
+++ components/footer.php
@@ -1,40 +1,46 @@
+<footer class="_bf_footer">
 
-
-<div class="_bf_footer">
-
-    <img class="rounded mx-auto d-block" src="img/DHSALUD-logo-small.png" alt="">
+    <img class="_bf_logo-footer rounded mx-auto d-block" src="img/DHSALUD-logo-small.png" alt="">
 
 
 
-    <div class=" _bf_datos-footer-horizontal">
+    <div class="row d-flex justify-content-center">
 
-        <div class="col d-flex justify-content-md-center">
+        <div class="col-12 col-md-4 d-flex justify-content-center pt-3 pb-3 _bf_ubicacion">
 
-            <div class="col-md-4 d-flex justify-content-end">
+            <a href="#" class="">
+                <img src="img/icons/ubicacion-icono.png" alt="">
+                Lorem ipsum dolor sit amet consectetur
+            </a>
 
-                <a href="#" class="_bf_ubicacion">
-                    <img src="img/icons/ubicacion-icono.png" alt="">
-                    Lorem ipsum dolor sit amet consectetur
-                </a>
+        </div>
 
-            </div>
 
-            <div class="col-md-2 ">
-                <div class="_bf_redes d-flex justify-content-center">
-                    <a href="#"><img class="_bf_espaciado-redes" src="img/icons/instagram.png" alt=""></a>
-                    <a href="#"><img class="_bf_espaciado-redes" src="img/icons/facebook.png" alt=""></a>
-                    <a href="#"><img class="_bf_espaciado-redes" src="img/icons/twitter.png" alt=""></a>
-                    <a href="#"><img src="img/icons/linkedin.png" alt=""></a>
-                </div>
-            </div>
+        <div class="col-12 col-md-4 d-flex justify-content-around pt-3 pb-3 _bf_redes">
 
-            <div class="col-md-4 ">
-                <a href="#" class="_bf_correo">
-                    <img src="img/icons/correo-icono.png" alt="">
-                    dhsalud@correomentira.com
-                </a>
+            <a href="#">
+                <img src="img/icons/instagram.png" alt="">
+            </a>
 
-            </div>
+            <a href="#">
+                <img src="img/icons/facebook.png" alt="">
+            </a>
+
+            <a href="#">
+                <img src="img/icons/twitter.png" alt="">
+            </a>
+
+            <a href="#">
+                <img src="img/icons/linkedin.png" alt="">
+            </a>
+
+        </div>
+
+        <div class="col-12 col-md-4 d-flex justify-content-center pt-3 pb-3 _bf_contacto">
+            <a href="#">
+                <img src="img/icons/correo-icono.png" alt="">
+                dhsalud@correomentira.com
+            </a>
 
         </div>
 
@@ -42,42 +48,7 @@
 
 
 
-    <div class="_bf_datos-footer-vetical ">
-        <div class="_bf_redes d-flex justify-content-center">
-            <a href="#"><img class="_bf_espaciado-redes" src="img/icons/instagram.png" alt=""></a>
-            <a href="#"><img class="_bf_espaciado-redes" src="img/icons/facebook.png" alt=""></a>
-            <a href="#"><img class="_bf_espaciado-redes" src="img/icons/twitter.png" alt=""></a>
-            <a href="#"><img src="img/icons/linkedin.png" alt=""></a>
-        </div>
-
-
-        <div class="row">
-
-
-            <div class="col-12 d-flex justify-content-center">
-                <a href="#" class="_bf_correo">
-                    <img src="img/icons/correo-icono.png" alt="">
-                    dhsalud@correomentira.com
-                </a>
-            </div>
-
-
-            <div class="col-12 d-flex justify-content-center">
-                <a href="#" class="_bf_correo">
-                    <img src="img/icons/ubicacion-icono.png" alt="">
-                    Lorem ipsum dolor sit amet consectetur
-                </a>
-            </div>
 
 
 
-
-        </div>
-
-
-
-    </div>
-
-
-
-</div>
+</footer>
