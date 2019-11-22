74ec91b1b98fb011f2c9db8a4ba118c9fcade79d
Bruno Fuentes <bruno.e.fuentes97@gmail.com>
2019-11-22 10:56:56 -0300

    MODIFIED: form.php

--- 003form.php
+++ 003form.php
@@ -1,12 +1,15 @@
+
+
+
 <?php
 
-  
+
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
 <head>
-<style> 
+<style>
 form{
 margin:  auto;
 width: 400px;
@@ -19,7 +22,7 @@ border-radius:7px;
 
 input{
 width:100%;
-margin-bottom: 20; 
+margin-bottom: 20;
 padding: 5px;
 box-sizing: border-box;
 font-size:17;
@@ -49,68 +52,70 @@ border: none;
 
 
         <div class="row d-flex flex-wrap justify-content-around">
-        <form action="form.php" method="POST">
-
-      
-      <input id="nombres" type="text" name="nombres" placeholder="Nombres">
-      <p></p>
-      <input id="Apellidos" type="text" name="apellidos" placeholder="Apellidos">
-      <p></p>
-      <input id="Edad" type="text" name="Edad" placeholder="Edad">
-      <p></p>
-      <input id="D.N.I." type="text" name="D.N.I." placeholder="D.N.I.">
-      <p></p>
-      <label for="Sexo">Sexo:</label> 
-      <select class="" name="Sexo" id="Sexo">
-      <option value="A">
-      Masculino
-      <option value="B">
-      Femenino
-      <option value="C">
-      Otros
-     <p></p>
-      <input id="Direccion" type="text" name="Direccion" placeholder="Direccion">
-      <p></p>
-      <input id="Localidad" type="text" name="Localidad" placeholder="Localidad">
-      <p></p>
-      <input id="Celular" type="text" name="Celular" placeholder="Celular">
-      <p></p>
-      <input id="email" type="email" name="email" placeholder="E-mail">
-      <p></p>
-      <label for="social">Obra Social:</label> 
-    
-      <select class="" name="social" id="social">
-      <option value="A">
-      Osep
-      <option value="B">
-      Galeno
-      <option value="C">
-      Cimesa
-      <option value="D">
-      Osde
-      <option value="F">
-      SancorSalud
-      <option value="G">
-      Ninguna
+            <form action="003form.php" method="POST">
 
 
-      </select>
-      <br>
-      <input id="enviar" type="submit" value="Enviar" >
-  
-</form>
+                <input id="nombres" type="text" name="nombres" placeholder="Nombres">
+                <p></p>
+                <input id="Apellidos" type="text" name="apellidos" placeholder="Apellidos">
+                <p></p>
+                <input id="Edad" type="text" name="Edad" placeholder="Edad">
+                <p></p>
+                <input id="D.N.I." type="text" name="D.N.I." placeholder="D.N.I.">
+                <p></p>
+                <label for="Sexo">Sexo:</label>
+                <select class="" name="Sexo" id="Sexo">
+                <option value="A">
+                Masculino
+                <option value="B">
+                Femenino
+                <option value="C">
+                Otros
+                <p></p>
+                <input id="Direccion" type="text" name="Direccion" placeholder="Direccion">
+                <p></p>
+                <input id="Localidad" type="text" name="Localidad" placeholder="Localidad">
+                <p></p>
+                <input id="Celular" type="text" name="Celular" placeholder="Celular">
+                <p></p>
+                <input id="email" type="email" name="email" placeholder="E-mail">
+                <p></p>
+                <label for="social">Obra Social:</label>
 
-           
+                <select class="" name="social" id="social">
+                    <option value="A">
+                    Osep
+                    <option value="B">
+                    Galeno
+                    <option value="C">
+                    Cimesa
+                    <option value="D">
+                    Osde
+                    <option value="F">
+                    SancorSalud
+                    <option value="G">
+                    Ninguna
+
+
+                </select>
+                <br>
+                <input id="enviar" type="submit" value="Enviar" >
+
+                <div class="">
+                    <p>¿Ya eres usuario?</p>
+                    <a href="">Click aqui</a>
+                </div>
+            </form>
+
 
         </div>
 
 
-
-
     </div>
 
-    <?php include_once "components/footer.php"; ?>
 
+
+    <?php include_once "components/footer.php"; ?>
     <?php include_once "components/scripts.php"; ?>
 </body>
 </html>
--- components/head.php
+++ components/head.php
@@ -6,13 +6,15 @@
     <!-- Css -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+
 
     <link rel="stylesheet" href="css/style-header.css">
     <link rel="stylesheet" href="css/style-chrome-body.css">
     <link rel="stylesheet" href="css/style-chrome-footer.css">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+
     <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
 
 
