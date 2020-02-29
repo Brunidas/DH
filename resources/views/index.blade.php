ab69a563b952310d9a02ae9b780c73e112bd11a1
Bruno Fuentes (Brunidas) <bruno.e.fuentes@gmail.com>
2020-02-29 15:19:04 -0300

    ADD: Rutas y index.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Css -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="/css/style-index.css">
+
+    <title>DH Salud</title>
+</head>
+<body>
+
+    <div class="container">
+
+
+        <!-- botones modo escritorio -->
+        <div class="row">
+            <div class="col d-flex justify-content-end ">
+                <div class="_bf_botones-escritorio d-none d-md-block">
+                    
+                    <a href=" {{ url('login') }} ">
+                        <button type="submit" class="_bf_boton-escritorio _bf_iniciar-sesion-escritorio">
+                            Iniciar Sesión
+                        </button>
+                    </a>
+
+                    <a href="{{ url('signup1') }}">
+                        <button class="_bf_boton-escritorio _bf_crear-cuenta-escritorio">Crear Cuenta</button>
+                    </a>
+
+                </div>
+
+            </div>
+        </div>
+    
+    
+    
+    
+        <div class="row">
+            <!-- LOGO -->
+            <div class="col d-flex justify-content-center">
+
+
+                <img class="_bf_logo " src="img/DHSALUD-logo.png" alt="">
+
+            </div>
+
+
+            <!-- boton inicio sesion modo mobile -->
+            <div class="col-12 mb-4 d-flex justify-content-center d-md-none">
+                <button class="_bf_boton-mobile _bf_boton-iniciarSesion-mobile ">
+                    Iniciar Sesión
+                </button>
+            </div>
+
+            <!-- boton crear cuenta modo mobile -->
+            <div class="col-12 mb-4 d-flex justify-content-center d-md-none">
+                <button class="_bf_boton-mobile _bf_boton-crearCuenta-mobile">
+                    Crear Cuenta
+                </button>
+            </div>
+
+            <!-- redes modo mobile -->
+            <div class="col-12 d-flex justify-content-center d-md-none">
+                <a href="">
+                    <img class="m-3" src="img/icons/instagram.png" alt="">
+                </a>
+                <a href="">
+                    <img class="m-3" src="img/icons/facebook.png" alt="">
+                </a>
+                <a href="">
+                    <img class="m-3" src="img/icons/twitter.png" alt="">
+                </a>
+                <a href="">
+                    <img class="m-3" src="img/icons/linkedin.png" alt="">
+                </a>
+            </div>
+
+            <!-- correo modo mobile -->
+            <div class="col-12 mt-3 d-flex justify-content-center d-md-none">
+                <a class="_bf_correo-mobile" href="">
+                    <img src="img/icons/correo-icono.png" alt="">
+                    dhsalud@correomentira.com
+                </a>
+            </div>
+
+        </div>
+    
+    
+
+        <!-- redes modo escritorio -->
+        <div class="row">
+            <div class="col d-flex justify-content-end">
+
+                <div class="_bf_redes-escritorio d-none d-md-block">
+                    <a href="#"><img src="img/icons/instagram.png" alt=""></a>
+                    <a href="#"><img src="img/icons/facebook.png" alt=""></a>
+                    <a href="#"><img src="img/icons/twitter.png" alt=""></a>
+                    <a href="#"><img src="img/icons/linkedin.png" alt=""></a>
+
+                </div>
+
+            </div>
+
+
+            <!-- correo modo escritorio -->
+            <div class="row">
+                <div class="col d-flex justify-content-end">
+                    <a class="_bf_correo-escritorio d-none d-md-block" href="#"><img src="img/icons/correo-icono.png" alt="">
+                        dhsalud@correomentira.com
+                    </a>
+
+                </div>
+            </div>
+
+
+
+        </div>
+
+
+    </div>
+
+    <!-- Java script -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,48 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', function(){
+    return view('index');
+});
+
+
+Route::get('login', function(){
+    return view('login');
+});
+
+Route::get('signup1', function(){
+    return view('signup1');
+});
+
+
+Route::get('signup2', function(){
+    return view('signup2');
+});
+
+Route::get('home', function(){
+    return view('home');
+});
+
+Route::get('faq', function(){
+    return view('faq');
+});
+
+Route::get('contact', function(){
+    return view('contact');
+});
+
+
+Route::get('profile', function(){
+    return view('profile');
+});
+
+
+
+Route::get('turn',function(){
+    return view('turn');
 });
